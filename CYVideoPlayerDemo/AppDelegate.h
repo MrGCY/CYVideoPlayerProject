61cb463432a571102ea3af1ff7607c72231e7ecb
gaochenyang <mr_gcy@163.com>
2017-09-06 16:16:28 +0800

    Initial Commit

---
+++ CYVideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CYVideoPlayerDemo
+//
+//  Created by Mr.GCY on 2017/9/6.
+//  Copyright © 2017年 Mr.GCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CYVideoPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CYVideoPlayerDemo
+//
+//  Created by Mr.GCY on 2017/9/6.
+//  Copyright © 2017年 Mr.GCY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
